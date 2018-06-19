88b610ea2ed538d4ece2b08a77151365d9c35366
anthony <anthony.synenko@gmail.com>
2018-06-19 10:59:41 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+//  connect express
+const app = express();
+//  register partials for handlebars view engine
+hbs.registerPartials(__dirname + '/views/partials');
+//  set handlebars view engine as a view rngine for our node js app
+app.set('view engine', 'hbs');
+
+//  middlewares
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', `${log}\n`, err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+//  static path
+app.use(express.static(__dirname + '/public'));
+
+//  helpers - you can use it in any place of hbs file
+hbs.registerHelper('getCurrentYear', () => (
+  new Date().getFullYear()
+));
+
+hbs.registerHelper('sceamIt', text => text.toUpperCase());
+
+//  http routes
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+//  listen port
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
+//  offtop
+//  for comfort development:
+//  nodemon server.js -e js,hbs
