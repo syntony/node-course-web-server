1d56b83bf488f64ae90297a0632d318e1cb67920
anthony <anthony.synenko@gmail.com>
2018-06-19 11:54:24 +0300

    add projects page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
 //  listen port
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
